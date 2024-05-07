9d70908519782528e0eeb89b1baa03b84b0601ac
Shoaib Rahman Shajid <shoaibrahmanshajid@gmail.com>
2024-05-07 16:48:15 +0600

    first_commit

--- eam_q_2.py
+++ eam_q_2.py
@@ -1,53 +1,45 @@
 class Circle:
     def __init__(self, radius):
         self.radius = radius
-
-    def __mul__(self, other):
-        if isinstance(other, Circle):
-            return 3.14 * self.radius**2
-        return NotImplemented
-
+    def __mul__(self):
+        return 3.14 * self.radius ** 2
+    
 class Rectangle:
     def __init__(self, length, width):
         self.length = length
         self.width = width
-
-    def __mul__(self, other):
-        if isinstance(other, Rectangle):
-            return self.length * self.width
-        return NotImplemented
-
+    def __mul__(self):
+        return self.length * self.width
+    
 class Triangle:
-    def __init__(self, base, height):
+    def __init__(self,base, height):
         self.base = base
         self.height = height
+    def __mul__(self):
+        return 0.5 * self.base * self.height
+    
 
-    def __mul__(self, other):
-        if isinstance(other, Triangle):
-            return 0.5 * self.base * self.height
-        return NotImplemented
-
-def calculate_area(shape):
-    return shape * shape
-
-def create_shape(shape_type):
-    if shape_type == 'circle':
-        radius = float(input("Enter the radius of the circle: "))
-        return Circle(radius)
-    elif shape_type == 'rectangle':
-        length = float(input("Enter the length of the rectangle: "))
-        width = float(input("Enter the width of the rectangle: "))
-        return Rectangle(length, width)
-    elif shape_type == 'triangle':
-        base = float(input("Enter the base of the triangle: "))
-        height = float(input("Enter the height of the triangle: "))
-        return Triangle(base, height)
+    
+def area(shape):
+    if shape == 'circle':
+        r = int(input("Enter the radius of circle: "))
+        area = Circle(r)
+        return area.__mul__()
+    elif shape == 'rectangle':
+        l = int(input("Enter the length of rectangle: "))
+        w = int(input("Enter the width of rectangle: "))
+        area = Rectangle(l,w)
+        return area.__mul__()
+    elif shape == 'triangle':
+        b = int(input("Enter the base of triangle: "))
+        h = int(input("Enter the height of triangle: "))
+        area = Triangle(b,h)
+        return area.__mul__()
     else:
-        return None
+        return "Invalid Shape"
 
-shape_type = input("Enter shape type (circle, rectangle, triangle): ")
-shape = create_shape(shape_type)
-if shape:
-    print('Area:', calculate_area(shape))
-else:
-    print('Invalid Shape')
+shape = input("Enter shape type(circle,rectangle,triangle): ")
+
+
+
+print(f"Area: {area(shape)}")
